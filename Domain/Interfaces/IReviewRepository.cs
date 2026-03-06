a4ac720f35753026db6e1644a4def90ac5409f57
Gustavo Marques Martins <gustavomqm@hotmail.com>
2026-03-06 16:14:16 -0300

    Review Repository adicionado

---
+++ Domain/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tracked.Domain.Entities;
+
+namespace Tracked.Domain.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<Review?> GetByIdsAsync(Guid usuarioId, Guid musicaId);
+        Task<IEnumerable<Review>> GetByMusicaIdAsync(Guid musicaId);
+        Task<Review> CreateAsync(Review review);
+        Task DeleteAsync(Guid usuarioId, Guid musicaId);
+        Task UpdateAsync(Review review);
+    }
+}
---
+++ Infrastructure/Repositories/ReviewRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tracked.Domain.Entities;
+using Tracked.Domain.Interfaces;
+using Tracked.Infrastructure.Context;
+
+namespace Tracked.Infrastructure.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        readonly AppDbContext _context;
+        public ReviewRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Review> CreateAsync(Review review)
+        {
+            await _context.Reviews.AddAsync(review);
+            await _context.SaveChangesAsync();
+            return review;
+        }
+
+        public async Task DeleteAsync(Guid usuarioId, Guid musicaId)
+        {
+            var review = await GetByIdsAsync(usuarioId, musicaId);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Review?> GetByIdsAsync(Guid usuarioId, Guid musicaId)
+        {
+            return await _context.Reviews
+                .FirstOrDefaultAsync(r => r.UsuarioId == usuarioId && r.MusicaId == musicaId);
+        }
+
+        public async Task<IEnumerable<Review>> GetByMusicaIdAsync(Guid musicaId)
+        {
+            return await _context.Reviews
+                .Where(r => r.MusicaId == musicaId)
+                .ToListAsync();
+        }
+
+        public async Task UpdateAsync(Review review)
+        {
+            _context.Reviews.Update(review);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
